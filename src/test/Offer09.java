3970095d0504f2bfa0868f7814eedf9f945decaf
wangxinyu <fqncita@dingtalk.com>
2022-11-15 16:32:23 +0800

    Offer09

--- src/test/Offer09.java
+++ src/test/Offer09.java
@@ -1,2 +1,35 @@
-package test;public class Offer09 {
+package test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead操作返回 -1 )
+ */
+public class Offer09 {
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    public Offer09() {
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        inStack.push(value);
+    }
+    
+    public int deleteHead() {
+        if (outStack.isEmpty()) {
+            if (inStack.isEmpty()) {
+                return -1;
+            }
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
 }
